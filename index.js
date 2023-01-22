15528891facbbb6f29cbda06271fa99c5062cd33
Francsy <francsyludens@gmail.com>
2023-01-22 19:20:10 +0100

    prueba 1

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/users', (req, res, next) => {
+    try {
+        const result = fs.readFileSync('db/users.json', 'utf8');
+        const dataBase = JSON.parse(result);
+        res.status(200).json(dataBase)
+    } catch (error) {
+        throw new Error('Error al obtener los datos de la base de datos');
+    }
 })
 
 
