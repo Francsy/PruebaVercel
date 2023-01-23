40508660b1875cfec1e426f6fd91757a041e57c0
Francsy <francsyludens@gmail.com>
2023-01-23 15:23:25 +0100

    prueba 10

--- index.js
+++ index.js
@@ -22,6 +22,38 @@ app.get('/users', (req, res, next) => {
 })
 
 
+app.post('/users', (req, res, next) => {
+  const { email, firstName, lastName, username } = req.body;
+  if (email && firstName && lastName && username) {
+      const newUser = {
+          id: uuidv4(),
+          email,
+          firstName,
+          lastName,
+          phone: req.body.phone || '',
+          img: req.body.img || '',
+          username,
+          address: req.body.address || {},
+          vehicles: req.body.vehicles || [],
+          favouritesFood: req.body.favouritesFood || [],
+          deleted: false
+      }
+      let dataBase = getData();
+      dataBase.push(newUser);
+      fs.writeFile(dbPath, JSON.stringify(dataBase), (err, data) => {
+          if (err) {
+              next(err);
+          } else {
+              console.log('Usuario guardado')
+              res.status(201).json({ success: true, message: `Usuario creado` })
+          }
+      })
+  } else {
+      res.status(400).json({ success: false, message: "Datos incompletos" })
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
